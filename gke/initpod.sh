58e737971b2fb0c1ab20e46dd33424974e56224d
Karel van IJperen <karel.van.ijperen@e-wise.nl>
2019-01-24 11:58:34 +0100

    Require health_check when not already in composer
    The require is very slow at some times so it is recommended to make sure
    the health_check module is installed from lockfile when GKE is uses.

--- gke/initpod.sh
+++ gke/initpod.sh
@@ -66,7 +66,10 @@ function install_dependencies {
 
 function install_health_check {
     logf '\n### Install and patch health check module ###\n'
-    logt composer require  'drupal/health_check:^1.0' --no-progress --optimize-autoloader
+    if ! grep -q '"drupal/health_check"' "$APP_ROOT/composer.json"
+    then
+        logt composer require  'drupal/health_check:^1.0' --no-progress --optimize-autoloader
+    fi
     # Small hack - add info the health output so we can see which specific POD is answering
     sed -i "s/response->setContent(time());/response->setContent(time() . ' TAG: ' . getenv('TAG') . ' HOSTNAME: ' . getenv('HOSTNAME'));/g" web/modules/contrib/health_check/src/Controller/HealthController.php
 }
