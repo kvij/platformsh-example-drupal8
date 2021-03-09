a118f5abbad7a3885987c5a86b8a3e0a081bae83
Karel van IJperen <hjk010@gmail.com>
2021-03-09 18:43:51 +0100

    Added automatic kubernetes context switch in helm deployments
    Accidental deployment to wrong cluster is prevented with this commit.
    deploy and uninstall targets have been replaced with deploy/all and uninstall/all
    targets. deploy/all might contain undesired environments to deploy to.

---
+++ scripts/helm/switch_context.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+project="kuberdrupal"
+region="europe-west4"
+
+environment="$1"
+if [[ "$1" == "" ]]; then
+    echo "Please select an environment:"
+    select environment in testing staging production
+    do
+        if [[ ${environment} != "" ]]; then
+            break
+        fi
+    done
+fi
+
+case "${environment}" in
+    "testing" | "staging")
+        context="gke_kuberdrupal_europe-west4_testing"
+        cluster="testing"
+        ;;
+    "production")
+        context="gke_kuberdrupal_europe-west4_production"
+        cluster="production"
+        ;;
+    *)
+        echo "Usage: ./$0 <testing|staging|production>"
+        exit 1
+        ;;
+esac
+
+echo "Ensuring context is set to ${environment}..."
+
+has_context=$(kubectl config get-contexts -o name | grep -e "^${context}$")
+if [[ "${has_context}" == "" ]]; then
+    gcloud container clusters get-credentials ${cluster} --project ${project} --region ${region} > /dev/null
+else
+    kubectl config use-context ${context} > /dev/null
+fi
